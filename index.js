f6b36271d9847359946fd2261f62509fedd834e9
gulfarooqi <54748795+gulfarooqi@users.noreply.github.com>
2019-08-31 20:39:41 -0700

    Added code for webcam loop

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let net;
+const webcamElement = document.getElementByID('webcam');
 
+// This is the 'app' function
 async function app() {
     console.log('Loading mobilenet..')
 
@@ -7,10 +9,38 @@ async function app() {
     net = await mobilenet.load();
     console.log('Sucessfully loaded model');
 
-    // Make a prediction through the model on our image.
-    const imgEl = document.getElementById('img');
-    const result = await net.classify(imgEl);
-    console.log(result);
+    await setupWebcam();
+    while(true) {
+        const result = await net.classify(webcamElement);
+        
+        document.getElementById('console').innerText = '
+        prediction: ${result[0].className}\n
+        probability: ${result[0].probability}
+    ';
+    
+    // Give some breathing room by waiting for the next animation frame to fire.
+    await tf.nextFrame();
+    }
+}
+
+// This is the setup webcam function which will get media from user webcam
+async function setupWebcam() {
+    return new Promise((resolve,reject) => {
+        const navigatorAny = navigator;
+        navigator.getUserMedia = navigator.getUserMedia ||
+            navigatorAny.webkitGetUserMedia || navigatorAny.mozGetuserMedia ||
+            navigatorAny.msGetUserMedia;
+        if (nagivator.getUserMedia) {
+            navigator.getUserMedia({video: true},
+                stream => {
+                    webcamElement.srcObject = strem;
+                    webcamElement.addEventListener('loadeddata', () => resolve(), false);
+            },
+            error => reject());
+        } else {
+            reject();
+        }
+    });
 }
 
 app();
